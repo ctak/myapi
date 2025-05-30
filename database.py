58978a40c22c5527166f77c7e11004dd6fd52f29
c tak <tak.changbeom@gmail.com>
2025-05-30 14:52:44 +0900

    2-05-2 스벨트 라우터 전

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+# import contextlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,3 +12,14 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# This function is used to get a database session.
+# It ensures that the session is closed after use, preventing resource leaks.
+# @contextlib.contextmanager # Depnds를 사용하기 때문에 주석 처리
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
---
+++ domain/question/question_router.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+# from database import SessionLocal
+from database import get_db
+# from models import Question
+
+from domain.question import question_schema, question_crud
+
+# p
+router = APIRouter(
+  prefix="/api/question",
+)
+
+@router.get("/list", response_model=list[question_schema.Question])
+def question_list(db: Session = Depends(get_db)):
+  ## SessionLocal을 사용하여 DB에 접근하는 방법
+  # db = SessionLocal()
+  # _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+  # db.close() # 이걸 꼭 해야하나 보지? => Pool에 돌려주는 것이니 괜찮다.
+  #
+  # # 제너레이터 사용
+  # with get_db() as db:
+  #     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+  #
+  # # Depends 사용
+  # but @contextlib.contextmanager 를 주석 처리해야 함.
+  # > FastAPI는 제너레이터 기반 함수를 직접 지원하며, 자동으로 리소스 관리를 처리한다. 
+  # > 그러나 @contextlib.contextmanager를 사용하면 get_db 함수가 contextlib._GeneratorContextManager 객체를 반환하게 되어 
+  # > FastAPI의 종속성 주입이 제대로 동작하지 않는다. 따라서 get_db 함수에서 @contextlib.contextmanager 어노테이션을 제거해야 한다
+  # 
+  
+  _question_list = question_crud.get_question_list(db)
+  return _question_list
